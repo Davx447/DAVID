2bc144132b4daf239e99f452de78bb9268620758
davx447 <davidfernandopasichana@gmail.com>
2024-02-22 12:09:29 -0500

    Metodos

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -3,9 +3,34 @@ class Empleado:
     """-------------------------------------
     #atributos
     ----------------------------------------"""
-    Nombrre=""
+    Nombre=""
     apellido=""
     ----------------------------------------------""
-"#1 masculino y 2 femenino"
-Sexo=0
-Salario=0
+    """-----------------------------------------------
+    #1 masculino y 2 femenino"
+    ------------------------------------------------"""
+    Sexo =0
+    Salario =0
+    '''-----------------------------------------------------------
+    # Metodos
+    -----------------------------------------------------------'''
+
+    def CambiarSalario(self, nuevoSalario):
+        # Aqui va el codigo de metodo
+        return 0
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo , nSalario):
+        # Aqui va el codigo del nuevo metodo
+        return None  
+    def ConsultarSalario(self):
+        # Aqui va el codigo del metodo
+        return self.Salario
+    def ConsultarNombre(self):
+        # Aqui va el codigo del metodo
+        return self.nombre
+    def ConsultarApellido(self):
+        # Aqui va el codigo del metodo
+        return self.apellido
+    def ConsultarNombreCompleto(self):
+        # Aqui va el codigo del metodo
+        return self.NombreCompleto
+    
