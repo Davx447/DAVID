2b9d5db7965c04070fa52d708b8f721694c002e3
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-02-28 12:20:02 -0500

    Update SimuladorBancario.py
    .

--- SimuladorBancario/SimuladorBancario.py
+++ SimuladorBancario/SimuladorBancario.py
@@ -1,8 +1,30 @@
+from CuentaAhorros import CuentaAhorros
+
 class SimuladorBancario:
-    #Aqui va el codigo del empleado
-    """-------------------------------------
-    #atributos
-    ----------------------------------------"""
-    Cedula=""
-    Nombres=""
-    MesActual=""
+    """
+    Atributos"""
+    cedula =0
+    nombre =0
+    mesActual =0
+    """---------------------------
+    Asociaciones
+    ------------------------------"""
+    SaldoCuentaCorriente=0
+    SaldoCuentaAhorros=0
+    SaldoTotal=0
+    """-----------------------------
+    Metodos
+    ------------------------------"""
+    def ConsignarCuentaCorriente(self, Cantidaddinero):
+        #Aqui va el codigo
+        MontoCuentaCorriente =+ Cantidaddinero
+        return 0
+    
+    def CalcularSaldoTotal(self):
+        #Aqui va el codigo
+        self.SaldoTotalotal= self.SaldoCuentaCorriente + self.SaldoCuentaAhorros
+        return "Saldo total es" +self.SaldoTotal
+    
+    def TransferirSaldoCuentaCorrienteaCuentaAhorros(self):
+        #Aqui va el codigo
+        
