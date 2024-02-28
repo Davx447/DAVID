481b5f9ace262a29831516d31f5cbb64857ff200
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-02-28 16:38:55 -0500

    Update SimuladorBancario.py
    .

--- SimuladorBancario/SimuladorBancario.py
+++ SimuladorBancario/SimuladorBancario.py
@@ -17,15 +17,29 @@ class SimuladorBancario:
     """-----------------------------
     Metodos
     ------------------------------"""
-    def ConsignarCuentaCorriente(self, Cantidaddinero):
+    def ConsignarSaldoCorriente(self, saldo):
         #Aqui va el codigo
-        MontoCuentaCorriente =+ Cantidaddinero
-        return 0
+        return self.SaldoCuentaCorriente.ConsignarValor(saldo)
     
     def CalcularSaldoTotal(self):
         #Aqui va el codigo
         SaldoTotal= self.SaldoCuentaCorriente + self.SaldoCuentaAhorros
-        return "Saldo total es" +self.SaldoCuentaAhorros + self. SaldoCuentaAhorros
+        return "Saldo total es" + SaldoTotal
     
-    def TransferirSaldoCuentaCorrienteaCuentaAhorros(self):
+    def TransferirSaldoCorrienteaAhorros(self, monto):
         #Aqui va el codigo
+        monto <= self.SaldoCuentaCorriente
+        self.SaldoCuentaCorriente -= monto
+        self.consignarSaldoAhorros(monto)
+        return monto
+    def ConsultarSaldoCorriente(self):
+        # Aqui va el codigo del metodo
+        return self.SaldoCuentaCorriente
+    def RetirarTodo(self):
+        # Aqui va el codigo
+        SaldoRetirado = self.SaldoCuentaAhorros + self.SaldocuentaAhorros
+        return "Su saldo retirado es:" + SaldoRetirado
+    def DuplicarAhorro(self): 
+        #Aqui va el codigo
+        self.SaldoCuentaAhorros *= 2
+        return self.DuplicarAhorro
