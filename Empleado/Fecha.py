7c3f819cd0869dd67529bf9dc7db41f620cb0c03
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-02-28 11:13:12 -0500

    Create Fecha.py
    .

---
+++ Empleado/Fecha.py
@@ -0,0 +1,23 @@
+class Fecha:
+    #codigo
+    '''-----------
+    Fecha
+    -------------'''
+    dia=""
+    mes=""
+    anio=""
+    """---------------------------------
+    # Metodos
+    ----------------------------------"""
+
+    def ConsultarDia(self):
+    # Aqui va el codigo del metodo
+        return self.dia
+
+    def ConsultarMes(self):
+    # Aqui va el codigo del metodo
+        return self.mes
+    
+    def ConsultarAnio(self):
+    # Aqui va el codigo del metodo
+        return self.anio
