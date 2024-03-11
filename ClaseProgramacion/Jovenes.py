e821351aa631de2df1cae424c951e55f15153539
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-03-11 12:55:13 -0500

    Create Jovenes.py
    s

---
+++ ClaseProgramacion/Jovenes.py
@@ -0,0 +1,12 @@
+    Juan = 20
+    Pedro = 10
+    Javier = 50
+    Junior = 1
+
+    print(Juan > Pedro)
+    print(Juan > Pedro < Junior)
+    print(Juan < Pedro > Junior)
+    print(Junior < Pedro < Juan < Javier)
+    print(Pedro + Junior < Juan)
+    print(Pedro + Junior <= Juan)
+    print(Juan - Junior == Pedro)
