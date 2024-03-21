e9fe90bb334fdbb7c6cafc4f4422dd1f7ce9a47e
Davx447 <160884743+Davx447@users.noreply.github.com>
2024-03-21 12:42:06 -0500

    Create tienda.py
    s

---
+++ Producto/tienda.py
@@ -0,0 +1,18 @@
+from producto import Producto
+
+class tienda:
+
+    """-----------------------------------------------------
+    #Atributos
+    ----------------------------------------------------"""
+
+    __producto1 = None
+    __producto2 = None
+    __producto3 = None
+    __producto4 = None
+    """--------------------------------------------------
+    #Metodos
+    ---------------------------------------------------"""
+
+    def getproducto1(self):
+        return self.__producto1
